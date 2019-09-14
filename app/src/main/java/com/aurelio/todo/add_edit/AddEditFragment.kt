e3646880fa54155fe6307836f4ffd20e627e71af
viniciosbastos <aurelio.vinicios1996@gmail.com>
2019-09-13 22:38:18 -0300

    Added IME action to keyboard and created listener to add a new ToDo after done button is pressed

--- app/src/main/java/com/aurelio/todo/add_edit/AddEditFragment.kt
+++ app/src/main/java/com/aurelio/todo/add_edit/AddEditFragment.kt
@@ -1,8 +1,10 @@
 package com.aurelio.todo.add_edit
 
+import android.opengl.Visibility
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.view.*
+import android.view.inputmethod.EditorInfo
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
@@ -22,8 +24,20 @@ class AddEditFragment : Fragment() {
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
 
+        binding.todoText.setOnFocusChangeListener { view, hasFocus ->  if (hasFocus) binding.doneCheck.visibility = View.VISIBLE}
+        binding.todoText.setOnEditorActionListener { view, actionId, keyEvent ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                addTodo()
+                true
+            }
+            false
+        }
         setHasOptionsMenu(true)
         return binding.root
+    }
+
+    private fun addTodo() {
+        Toast.makeText(context, "ToDo added", Toast.LENGTH_LONG).show()
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
